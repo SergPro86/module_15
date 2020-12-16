403a2bf7ed572ff7c8f84c638a591815aa16889e
Sergey <prosviryakov.sergey@gmail.com>
2020-12-16 17:38:00 +0600

    first commit

---
+++ knowledge.inc.php
@@ -0,0 +1,24 @@
+<?php
+  $a = 'Мои знания на';
+  $b = 100;
+  $c = '%'; 
+?>
+
+<?php
+  $price = 15;
+
+  if($price >= 15 && $price <= 20)
+      $d = 'Это число находится между 15 и 20';
+?>
+
+<?php 
+  $e = __FILE__;
+?>
+
+<?php 
+  $start = microtime(true); 
+?>
+
+<?php 
+  $start = microtime(true); 
+?>
